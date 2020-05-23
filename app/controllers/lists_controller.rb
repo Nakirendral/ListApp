65b65e3b28b9d429b94c21cffbe24b534bfb4bbc
Sid Montoya <sidneymontoya@gmail.com>
2020-05-22 22:02:42 -0400

    Initializing repository.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,14 +2,25 @@ class ListsController < ApplicationController
 #before_action :find_list, only: [:show, :update, :edit, :destroy]
 
   def index
-    #@lists = List.all.order("created_at DESC")
+    @lists = List.all.order("created_at DESC")
   end
 
   def new
-
+    @list = List.new
   end
 
   def create
-  
+    @list = List.new(list_params)
+
+    if @post.save
+      redirect_to @list
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :content)
   end
 end
