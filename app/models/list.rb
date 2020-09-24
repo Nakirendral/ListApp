59031afdcefe95983b1ad4215bb6ce737a39ac5e
Sid Montoya <sidneymontoya@gmail.com>
2020-09-24 13:14:03 -0400

    Fixed the issues plaguing the http://localhost:3000/lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  has_many :entries, dependent :destroy
+  has_many :entries, dependent: :destroy
   belongs_to :user
   validates :list_name, length: { maximum: 140 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,36 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_034749) do
+ActiveRecord::Schema.define(version: 2020_09_23_005148) do
 
   create_table "entries", force: :cascade do |t|
-    t.text "entry_name"
-    t.integer "list_id"
-    t.integer "votes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lists", force: :cascade do |t|
-    t.text "list_name"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
