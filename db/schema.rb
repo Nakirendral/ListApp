ca04639a0b0ba0133ccc2dacb9db5550a8ff0533
Sid Montoya <sidneymontoya@gmail.com>
2020-10-05 18:06:38 -0400

    Removed ListId:integer from [Lists] Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_165120) do
+ActiveRecord::Schema.define(version: 2020_10_05_220547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_10_02_165120) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "listid"
   end
 
   create_table "users", force: :cascade do |t|
