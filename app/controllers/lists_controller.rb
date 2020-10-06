fc8e747a39fb5119618b1ea94404bd3069544966
Sid <sidneymontoya@gmail.com>
2020-10-06 11:09:35 -0400

    Update lists_controller.rb

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,12 +16,12 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
 
-    if @list.save
-      redirect_to @list
-    # else
-    #   render 'new'
-    end
+  if @list.save
+    redirect_to @list
+  else
+    render 'new'
   end
+end
 
   private
   def list_params
