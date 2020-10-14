22974f2b93af37b6f77ee9168bb6f50c007085a7
Sid Montoya <sidneymontoya@gmail.com>
2020-10-14 11:41:52 -0400

    Added the model test to the corrent model test folder

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create entry associated with a list which is then associated with a user" do
+    entry = Entry.new
+    assert_not entry.save, "Saved the entry without reference to list"
+  end
 end
