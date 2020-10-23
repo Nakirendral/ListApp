b1e7fe0f739bb4aa3570cfda905655bf90167369
Sid Montoya <sidneymontoya@gmail.com>
2020-10-23 11:42:44 -0400

    Added a negative test to EntriesController

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -13,5 +13,19 @@ RSpec.describe EntriesController, type: :controller do
       get :show, params: {list_id: 1, id: 1}
       expect(response).to render_template('show')
     end
+
+    it "returns a list with corresponding list_id" do
+      user = User.create!(id: 1, username: "Squid", email: "s@s.com", first_name: "Sid", last_name: "Monty")
+      list = List.create!(id: 2, title: "heyo", user: user)
+      get :show, params: {list_id: list.id, id: 1}
+      expect(assigns(:list)).to eq(list)
+    end
+
+    it "does not return a list with a different list_id" do
+      user = User.create!(id: 1, username: "Squid", email: "s@s.com", first_name: "Sid", last_name: "Monty")
+      list = List.create!(id: 2, title: "heyo", user: user)
+      get :show, params: {list_id: 1, id: 1}
+      expect(assigns(:list)).not_to eq(list)
+    end
   end
 end
